42c502a91a30db60094f3881e8c814f80b9eb1a1
Ricardo Almeida <penumbroso@gmail.com>
2019-06-05 21:34:18 -0300

    Removed tests.

--- app/src/test/java/io/schiar/tcc/mock/MockBitmapLoaderFactory.kt
+++
@@ -1,9 +0,0 @@
-package io.schiar.tcc.mock
-
-import io.schiar.tcc.mock.GenericMockFactory.Companion.mock
-import io.schiar.tcc.utilities.BitmapLoader
-import io.schiar.tcc.utilities.BitmapLoaderFactoryInterface
-
-object MockBitmapLoaderFactory : BitmapLoaderFactoryInterface {
-    override fun bitmapLoader(urlString: String): BitmapLoader = mock()
-}
